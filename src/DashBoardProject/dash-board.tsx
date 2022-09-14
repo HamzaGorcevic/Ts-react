3f4a92df397b547a96fcc29314d235feb09365eb
Hamza Gorcevic <88393813+HamzaGorcevic@users.noreply.github.com>
2022-09-14 19:20:25 +0200

    changes

--- src/DashBoardProject/dash-board.tsx
+++ src/DashBoardProject/dash-board.tsx
@@ -3,37 +3,36 @@ import { ContainerStatistic, DashSingleStyle } from "./style-dash";
 import { Global } from "./style-dash";
 import axios from "axios";
 const DashApp: React.FC = () => {
-
-  const [data, setData] = useState<number[]>([])
-  const [value, setValue] = useState<number>(0)
-  const[arr,setArr] = useState<string[]>([])
+  const [data, setData] = useState<number[]>([]);
+  const [value, setValue] = useState<number>(0);
+  const [arr, setArr] = useState<string[]>([]);
   console.log("hell");
   useEffect(() => {
-    axios.get('http://www.randomnumberapi.com/api/v1.0/random?min=10&max=100&count=5').then((response) => {
-      setData(response.data)
-
-    })
-  }, [])
-
-
-  
+    axios
+      .get(
+        "http://www.randomnumberapi.com/api/v1.0/random?min=10&max=100&count=5"
+      )
+      .then((response) => {
+        setData(response.data);
+      });
+  }, []);
 
   return (
     <>
       <Global>
         <ContainerStatistic>
-
           {data.map((el) => {
-            return <DashSingleStyle el={el}>
-
-              {el}
-        
-            </DashSingleStyle>
+            return (
+              <DashSingleStyle el={el}>
+                {el}
+                <p>Hello World</p>
+              </DashSingleStyle>
+            );
           })}
         </ContainerStatistic>
       </Global>
     </>
-  )
+  );
 };
 
-export default DashApp
+export default DashApp;
